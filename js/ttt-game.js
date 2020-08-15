7274f0d4da1ccd762313e124e214451e77dfcab1
lilygruhl <63130871+lilygruhl@users.noreply.github.com>
2020-08-15 12:41:09 -0700

    Create ttt-game.js

---
+++ js/ttt-game.js
@@ -0,0 +1,22 @@
+function preload() {
+    
+}
+
+function setup() {
+    createCanvas(1000, 1000);
+}
+
+function draw() {
+    background(255,255,255);
+    
+}
+
+
+
+function keyPressed() {
+  
+}
+
+function mousePressed() {
+    
+}
